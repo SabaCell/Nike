c400eb0ab3e0e70a738706f7ba22c6f46dcf4601
Arsalan Valoojerdi <a.valoojerdi@gmail.com>
2020-10-31 11:23:42 +0330

    Add NonCacheable query base and query handler

--- Nike.Mediator/Query/ICachableQuery.cs
+++ Nike.Mediator/Query/ICachableQuery.cs
@@ -1,5 +1,5 @@
-using System;
 using Enexure.MicroBus;
+using System;
 
 namespace Nike.Mediator.Query
 {
---
+++ Nike.Mediator/Query/QueryBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Nike.Mediator.Query
+{
+    public abstract class QueryBase<TResult> : INonCacheableQuery<QueryBase<TResult>, TResult>
+        where TResult : class
+    {
+    }
+}
