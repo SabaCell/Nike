787df18a6259161c969a55b012640e4fb94dc64a
arsalan <a.valoojerdi@gmail.com>
2020-12-06 21:42:29 +0330

    Add event sourcing framework with sample

--- Nike.Framework.Domain.EventSourcing/HashCode.cs
+++ Nike.Framework.Domain.EventSourcing/HashCode.cs
@@ -1,68 +1,26 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="HashCode.cs" company="Souran">
-//   Souran copyright
-// </copyright>
-// <summary>
-//   Defines the HashCode type.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Nike.Framework.Domain.EventSourcing
 {
-    /// <summary>
-    /// کد مخدوش شده
-    /// </summary>
     public struct HashCode
     {
-        /// <summary>
-        /// مقدار اولیه-زمان تعریف
-        /// </summary>
         public const int InitialValue = 31;
 
-        /// <summary>
-        /// مقدار ضریب تولید مقادیر مخدوش شده ی ثانویه
-        /// </summary>
         public const int MultiplyerValue = 59;
 
-        /// <summary>
-        /// مقدار ضریب تولید مقادیر مخدوش شده ی ثانویه ی جایگزین
-        /// </summary>
         public const int AlternativeMultiplyerValue = 114;
 
-        /// <summary>
-        /// The hash code.
-        /// </summary>
         private readonly int hashCode;
 
-        /// <summary>
-        /// ایجاد کد مخدوش شده
-        /// </summary>
-        /// <param name="hashCode">مقدار مخدوش شده</param>
         public HashCode(int hashCode)
         {
             this.hashCode = hashCode;
         }
 
-        /// <summary>
-        /// ارائه ی کد مخدوش آغازین.
-        /// </summary>
         public static HashCode Start { get; } = new HashCode(InitialValue);
 
-        /// <summary>
-        /// تعریف عملگر ارائه ی مقدار کد مخدوش
-        /// </summary>
-        /// <param name="hashCode">کد مخدوش</param>
         public static implicit operator int(HashCode hashCode) => hashCode.GetHashCode();
 
-        /// <summary>
-        /// ارائه ی کد مخدوش با در نظر گرفتن یک موجودیت جدید
-        /// </summary>
-        /// <typeparam name="T">نوع موجودیت جدید</typeparam>
-        /// <param name="item">موجودیت/فیلد جدید</param>
-        /// <param name="useAlternativeMultiplyerValue">The Alternative Multiplyer Value.</param>
-        /// <returns>کد مخدوش جدید</returns>
         public HashCode WithHash<T>(T item, bool useAlternativeMultiplyerValue = false)
         {
             var hashValue = EqualityComparer<T>.Default.GetHashCode(item);
@@ -76,14 +34,6 @@ namespace Nike.Framework.Domain.EventSourcing
             return result;
         }
 
-        /// <summary>
-        /// ارائه ی کد مخدوش با در نظر گرفتن یک موجودیت جدید
-        /// </summary>
-        /// <typeparam name="T">نوع موجودیت جدید</typeparam>
-        /// <param name="item">موجودیت/فیلد جدید</param>
-        /// <param name="useAlternativeMultiplyer">استفاده از ضریب جایگزین</param>
-        /// <param name="changeMultiplyerValues">تغییر متناوب مقدار ضرایب</param>
-        /// <returns>کد مخدوش جدید</returns>
         public HashCode WithHashProperties<T>(T item, bool useAlternativeMultiplyer = false, bool changeMultiplyerValues = false)
         {
             var properties = this.GetType().GetProperties();
@@ -101,10 +51,6 @@ namespace Nike.Framework.Domain.EventSourcing
             return result;
         }
 
-        /// <summary>
-        /// ارائه ی مقدار کد مخدوش
-        /// </summary>
-        /// <returns>کد مخدوش جاری</returns>
         public override int GetHashCode() => this.hashCode;
     }
 }
