2c3496fb4355da036aff99e87dd35e7770d55d13
arsalan <a.valoojerdi@gmail.com>
2021-09-05 16:22:44 +0430

    Check isReplyAble in fail message

--- Nike.EventBus.Kafka/Model/ConsumeMessageResult.cs
+++ Nike.EventBus.Kafka/Model/ConsumeMessageResult.cs
@@ -94,15 +94,14 @@ namespace Nike.EventBus.Kafka.Model
                     await mediator.PublishAsync(message);
 
                     if (message.IsReplyAble)
-                    {
                         await bus.PublishAsync(MessageProcessResultIntegrationEvent.Success(message.Id), cancellationToken);
-                    }
                 }
                 catch (Exception exception)
                 {
-                    await bus.PublishAsync(MessageProcessResultIntegrationEvent.Fail(message.Id, exception.Message), cancellationToken);
+                    if (message.IsReplyAble)
+                        await bus.PublishAsync(MessageProcessResultIntegrationEvent.Fail(message.Id, exception.Message), cancellationToken);
 
-                    logger.LogError($"Consumed a message : {_topic} failed", exception);
+                    logger.LogError($"Consumed a message : {_topic} failed : {exception.Message}", exception);
                 }
                 finally
                 {
