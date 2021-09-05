40ee408cea22c3ac26df07c46f7a697eb3d2840a
Mostafa Feizabadi <feizabadi@sabacell.net>
2021-09-05 15:49:43 +0430

    chnage json log console

--- Nike.Logging/ConsoleLoggerExtensions.cs
+++ Nike.Logging/ConsoleLoggerExtensions.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Nike.Logging.NikeLog;
+using Microsoft.Extensions.Logging.Console;
 using Nike.Logging.Serilog;
 using Serilog.Extensions.Logging;
 
@@ -9,16 +9,18 @@ namespace Nike.Logging
 {
     public static class ConsoleLoggerExtensions
     {
-        public static ILoggingBuilder AddJsonConsole(this ILoggingBuilder builder)
+        public static ILoggingBuilder AddNikeJsonConsole(this ILoggingBuilder builder,
+            Action<JsonConsoleFormatterOptions> configure)
         {
-            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, NikeJsonLoggerProvider>());
+            builder.AddJsonConsole(configure);
 
             return builder;
         }
 
         public static ILoggingBuilder AddSerilogJsonConsole(this ILoggingBuilder builder)
         {
-            builder.Services.AddSingleton<ILoggerProvider, SerilogLoggerProvider>(services => new SerilogLoggerProvider(SerilogConfigurator.Config(), true));
+            builder.Services.AddSingleton<ILoggerProvider, SerilogLoggerProvider>(services =>
+                new SerilogLoggerProvider(SerilogConfigurator.Config(), true));
 
             return builder;
         }
