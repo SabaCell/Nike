9464c4c438a5a8025b90bd2f316dca38290f9d41
Mostafa Feizabadi <feizabadi@sabacell.net>
2021-09-06 22:33:59 +0430

    ass is Async Consumer

--- Nike.EventBus.Kafka/IKafkaConsumerConnection.cs
+++ Nike.EventBus.Kafka/IKafkaConsumerConnection.cs
@@ -5,6 +5,7 @@ namespace Nike.EventBus.Kafka
     public interface IKafkaConsumerConnection
     {
         bool IsConnected { get; }
+        bool IsAsync { get; }
         public int MillisecondsTimeout { get; set; }
         ConsumerConfig Config { get; }
         int StatisticsIntervalMs { get; set; }
