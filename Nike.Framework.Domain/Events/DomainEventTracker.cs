e3f871700bf73331fb256a0a4098935f390f3c85
mostafa <mostafaFeizabadi@gmail.com>
2022-04-25 19:58:25 +0430

    feat: replace concurrentQueue with list for DomainEvents

--- Nike.Framework.Domain/Events/DomainEventTracker.cs
+++ Nike.Framework.Domain/Events/DomainEventTracker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -6,26 +7,40 @@ namespace Nike.Framework.Domain.Events;
 
 public static class DomainEventTracker
 {
-    private static readonly List<Tuple<DomainEvent, CommitTime>> Events = new();
+    private static readonly ConcurrentQueue<Tuple<DomainEvent, CommitTime>> Events= new();
 
     public static void AddEvent(DomainEvent domainEvent, CommitTime commitTime)
     {
         if (commitTime.HasFlag(CommitTime.BeforeCommit))
-            Events.Add(new Tuple<DomainEvent, CommitTime>(domainEvent, CommitTime.BeforeCommit));
+            Events.Enqueue(new Tuple<DomainEvent, CommitTime>(domainEvent, CommitTime.BeforeCommit));
 
         if (commitTime.HasFlag(CommitTime.AfterCommit))
-            Events.Add(new Tuple<DomainEvent, CommitTime>(domainEvent, CommitTime.AfterCommit));
+            Events.Enqueue(new Tuple<DomainEvent, CommitTime>(domainEvent, CommitTime.AfterCommit));
     }
 
     public static List<DomainEvent> GetAllEvents(CommitTime commitTime = CommitTime.BeforeCommit)
     {
         var events = new List<DomainEvent>();
 
-        var allEvents = Events.Where(p => p.Item2.HasFlag(commitTime)).ToList();
+        while (true)
+        {
+            if (!Events.TryDequeue(out Tuple<DomainEvent, CommitTime> @event))
+            {
+                break;
+            }
 
-        events = allEvents.Select(m => m.Item1).ToList();
-
-        allEvents.ForEach(e => Events.Remove(e));
+            if (@event.Item2.HasFlag(commitTime))
+            {
+                events.Add(@event.Item1);
+            }
+            else
+            {
+                Events.Enqueue(@event);
+            }
+        }
+        // var allEvents = Events.Where(p => p.Item2.HasFlag(commitTime)).ToList();
+        // events = allEvents.Select(m => m.Item1).ToList();
+        // allEvents.ForEach(e => Events.Remove(e));
 
         return events;
     }
